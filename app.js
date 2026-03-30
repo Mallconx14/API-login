6d59b32b93fca1fe1e632fe6c60465d9ebe9bd8e
Mallconx14 <mallconx14@gmail.com>
2026-03-30 09:14:55 -0300

    corrigindo erros

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 //Rotas
-const userRoutes = require('.routes/users');
+const userRoutes = require('./routes/users');
 app.use('./users', userRoutes);
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const userRoutes = express.Router();
+const router = express.Router();
 const db = require('../db');
 
 //Get em usuários
-userRoutes.get('/', (req, res) => {
+router.get('/', (req, res) => {
     db.query('SELECT * FROM users', (err, results) => {
         if (err) {
             res.status(500).json({ error: 'Erro ao buscar usuários' });
