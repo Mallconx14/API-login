10b7a2b263308eb4e6ea7fca0dd78fbf74d49f9d
Mallconx14 <mallconx14@gmail.com>
2026-03-30 08:52:16 -0300

    criação da conexão ao db

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const router = express.Router();
+const userRoutes = express.Router();
 const db = require('../db');
 
 //Get em usuários
-router.get('/', (req, res) => {
+userRoutes.get('/', (req, res) => {
     db.query('SELECT * FROM users', (err, results) => {
         if (err) {
             res.status(500).json({ error: 'Erro ao buscar usuários' });
