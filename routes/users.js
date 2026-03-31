b6e2b739f84b09848dc5bb38f260e5950eeded24
Mallconx14 <mallconx14@gmail.com>
2026-03-31 16:42:59 -0300

    post e put completos

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,20 @@ const router = express.Router();
 const db = require('../db');
 
 //Post em usuários (create)
-
+router.post('/', (req, res) => {
+    const { nome, email, senha } = req.body;
+    db.query('INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)', [nome, email, senha], (err, results) => {
+        if (err) {
+            res.status(500).json({ error: 'Erro ao inserir indormações'});
+        } else {
+            res.json(results);
+        }
+    });
+});
 
 //Get em usuários (read)
 router.get('/', (req, res) => {
-    db.query('SELECT * FROM users', (err, results) => {
+    db.query('SELECT * FROM usuarios', (err, results) => {
         if (err) {
             res.status(500).json({ error: 'Erro ao buscar usuários' });
         } else {
@@ -16,6 +25,17 @@ router.get('/', (req, res) => {
     });
 }); 
 
-
+//Put em usuários (update)
+router.put('/edit/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, email, senha } = req.body;
+    db.query('UPDATE usuarios SET nome = ?, email = ?, senha =? WHERE id = ?', [nome, email, senha, id], (err, results) => {
+        if (err) {
+            res.status(500).json({ error: 'Erro ao batualizar informações' });
+        } else {
+            res.json(results);
+        }
+    })
+})
 
 module.exports = router;
